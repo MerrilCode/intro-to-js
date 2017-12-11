84bf56c62505df5cac87a604e8944c08ff61a3da
Tech-A67 <tech-a67@Tech-A67s-MacBook-Air.local>
2017-12-11 14:27:22 +0000

    javascript control flow

---
+++ js-control-flow/app.js
@@ -0,0 +1,37 @@
+
+//A simple everyday if
+
+var  thingWotIsTrue = false;
+if (thingWotIsTrue) {
+	alert("dwag");
+}
+
+//Also in one line
+if(thingWotIsTrue) alert("one line");
+
+var dolla = parseInt(prompt("How much money on ya?"));
+
+var drink = 0;
+var crisp = false;
+//for(i=0; i<3; i++){
+	if(dolla > 10 && drink <3){
+	console.log("I will buy another drink");
+	//drink++;
+	} else if (dolla >5 || crisp == true){
+		console.log("Buy some crisp");
+	}
+
+	else {
+		console.log("I'll go home!");
+	}
+//}
+
+console.log(drink);
+
+// comparisons (>,<, <=,>=,==,===)
+
+
+
+
+
+
